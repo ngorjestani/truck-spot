d9cde10ca0f49d85340106e54fc25360c005c82f
Nick Gorjestani <nickg2794@hotmail.com>
2021-11-16 23:02:59 -0600

    Started navbar

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,11 +1,43 @@
 import {FunctionComponent} from "react";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
-type NavMenuProps = {
-
-}
+type NavMenuProps = {}
 
 export const NavMenu : FunctionComponent<NavMenuProps> = (props) => {
     return (
-        <div></div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to='/'>
+                    <img
+                        src="../../public/logo.svg"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="TruckSpot logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
