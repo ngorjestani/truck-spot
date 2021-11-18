b83428ba70fef349d1c81ee94c02ce1eef95075b
Nick Gorjestani <nickg2794@hotmail.com>
2021-11-17 18:05:45 -0600

    Updated navbar links

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -6,35 +6,26 @@ type NavMenuProps = {}
 
 export const NavMenu : FunctionComponent<NavMenuProps> = (props) => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="transparent">
             <Container>
                 <Navbar.Brand as={Link} to='/'>
                     <img
-                        src="../../public/logo.svg"
-                        width="30"
-                        height="30"
+                        src="../logo.svg"
+                        width="50"
+                        height="50"
                         className="d-inline-block align-top"
                         alt="TruckSpot logo"
+                        style={{margin: 0}}
                     />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#pricing">About</Nav.Link>
+                        <Nav.Link as={Link} to='add'>Add your Truck</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
+                        <Nav.Link as={Link} to='login'>Sign In</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
