e5f264d9764cbfa8c0786b0e97c2d057059b8882
Nick Gorjestani <nickg2794@hotmail.com>
2021-11-18 22:32:30 -0600

    Home page initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import {FunctionComponent} from "react";
+import {Col, Container, Form, Image, Row} from "react-bootstrap";
 
 type HomeProps = {
 
@@ -6,6 +7,19 @@ type HomeProps = {
 
 export const Home : FunctionComponent<HomeProps> = (props) => {
     return (
-        <div></div>
+        <Container fluid>
+            <Row className='mt-4'>
+                <Col xs={{span: 8, offset: 2}} md={{span: 4, offset: 4}}>
+                    <Image src='./logo.svg' width='100%' fluid />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Form.Group>
+                        <Form.Control type='input' placeholder='Search...' className='rounded-pill' />
+                    </Form.Group>
+                </Col>
+            </Row>
+        </Container>
     )
 }
