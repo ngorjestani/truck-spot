15aac769397b35fe265f0ef01897cddedd6b146e
Nick Gorjestani <nickg2794@hotmail.com>
2021-11-21 16:30:37 -0600

    Sign in form

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,4 +1,6 @@
 import {FunctionComponent} from "react";
+import {Button, Container, Form} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 type SignInFormProps = {
 
@@ -6,6 +8,24 @@ type SignInFormProps = {
 
 export const SignInForm : FunctionComponent<SignInFormProps> = (props) => {
     return (
-        <div></div>
+        <Container>
+            <Form className='bg-light p-3 rounded shadow'>
+                <Form.Group className='mb-2'>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control type='email' placeholder='Email' className='rounded-pill' />
+                </Form.Group>
+                <Form.Group className='my-2'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type='password' placeholder='Password' className='rounded-pill' />
+                </Form.Group>
+                <Form.Text className='text-muted'>
+                    Don't have an account? <Link to='create'>Create an account</Link>
+                </Form.Text>
+                <br />
+                <Button variant='primary' type='submit' className='mt-2 rounded-pill shadow-sm'>
+                    Submit
+                </Button>
+            </Form>
+        </Container>
     )
 }
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,6 @@
 import {FunctionComponent} from "react";
+import {Col, Container, Image, Row} from "react-bootstrap";
+import {SignInForm} from "../components/SignInForm";
 
 type SignInProps = {
 
@@ -6,6 +8,17 @@ type SignInProps = {
 
 export const SignIn : FunctionComponent<SignInProps> = (props) => {
     return (
-        <div></div>
+        <Container fluid>
+            <Row className='mt-4'>
+                <Col xs={{span: 4, offset: 4}} md={{span: 2, offset: 5}}>
+                    <Image src='./logo.svg' width='100%' fluid />
+                </Col>
+            </Row>
+            <Row className='mt-5'>
+                <Col md={{span: 8, offset: 2}}>
+                    <SignInForm />
+                </Col>
+            </Row>
+        </Container>
     )
 }
