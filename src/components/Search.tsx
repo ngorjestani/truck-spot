4a01ce9b7d0fc5b467948deef8844815cfff1560
Nick Gorjestani <nickg2794@hotmail.com>
2021-11-24 18:57:01 -0600

    Updated search with autocomplete

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,18 +1,29 @@
-import {FunctionComponent} from "react";
+import {FunctionComponent, RefObject} from "react";
 import {Button, Form, InputGroup} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import {usePlacesWidget} from "react-google-autocomplete";
 
 type SearchProps = {
 
 }
 
 export const Search : FunctionComponent<SearchProps> = () => {
+    const { ref } = usePlacesWidget<HTMLInputElement>({
+        apiKey: process.env.GOOGLE_MAPS_API_KEY,
+        onPlaceSelected: (place) => {
+            console.log(place);
+    }
+    });
+
+    console.log(process.env.GOOGLE_MAPS_API_KEY)
+
     return (
         <Form.Group>
             <InputGroup>
                 <Form.Control
                     type='input'
+                    ref={ref}
                     placeholder='Search...'
                     className='rounded-pill'
                 />
