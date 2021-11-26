1ca7dd57e89d068accff6ae403dce015571d6c09
Nick Gorjestani <nickg2794@hotmail.com>
2021-11-25 22:16:53 -0600

    Added props to search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,20 @@
-import {FunctionComponent, RefObject} from "react";
+import {FunctionComponent, RefObject, useState} from "react";
 import {Button, Form, InputGroup} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faSearch} from "@fortawesome/free-solid-svg-icons";
 import {usePlacesWidget} from "react-google-autocomplete";
 
-type SearchProps = {}
+type SearchProps = {
+    searchValue: google.maps.places.PlaceResult,
+    handlePlaceSelected: (place: google.maps.places.PlaceResult) => void,
+    handleSubmit: () => void,
+}
 
-export const Search: FunctionComponent<SearchProps> = () => {
+export const Search: FunctionComponent<SearchProps> = ({searchValue,handlePlaceSelected, handleSubmit}) => {
     const {ref} = usePlacesWidget<HTMLInputElement>({
         apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
         onPlaceSelected: (place) => {
-            console.log(place);
+            handlePlaceSelected(place);
         },
         options: {
             types: ['address'],
@@ -21,16 +25,15 @@ export const Search: FunctionComponent<SearchProps> = () => {
     console.log(process.env.GOOGLE_MAPS_API_KEY)
 
     return (
-        <Form.Group>
+        <Form onSubmit={handleSubmit}>
             <InputGroup>
                 <Form.Control
                     type='input'
                     ref={ref}
                     placeholder='Search...'
-                    className='rounded-pill'
                 />
-                <Button><FontAwesomeIcon icon={faSearch}/></Button>
+                <Button type='submit'><FontAwesomeIcon icon={faSearch}/></Button>
             </InputGroup>
-        </Form.Group>
+        </Form>
     )
 }
