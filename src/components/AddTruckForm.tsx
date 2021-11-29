eb4a3cfb7ed2139968b7eaab2be3989a8dd769dd
Nick Gorjestani <nickg2794@hotmail.com>
2021-11-28 20:11:00 -0600

    Created add truck form

--- src/components/AddTruckForm.tsx
+++ src/components/AddTruckForm.tsx
@@ -1,11 +1,103 @@
-import {FunctionComponent} from "react";
+import {FunctionComponent, useRef, useState} from "react";
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
+import {usePlacesWidget} from "react-google-autocomplete";
+import {cuisines} from "../models/Cuisines";
 
 type AddTruckFormProps = {
 
 }
 
 export const AddTruckForm : FunctionComponent<AddTruckFormProps> = (props) => {
+    const [inputState, setInputState] = useState({
+
+    });
+    const {ref} = usePlacesWidget<HTMLInputElement>({
+        apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        onPlaceSelected: (place) => {
+            console.log(place);
+        },
+        options: {
+            types: ['address'],
+        }
+    });
+    const getCuisines = () => {
+        return (
+            <Form.Select>
+                {cuisines.map((cuisine, id) => (
+                    <option value={cuisine.id}>{cuisine.value}</option>
+                ))}
+            </Form.Select>
+        );
+    };
+    const inputRef = useRef<HTMLInputElement>(null);
+    const handleUpload = (e: React.MouseEvent) => {
+        e.preventDefault();
+        inputRef.current?.click();
+    };
+
     return (
-        <div></div>
+        <Container>
+            <Form className='bg-light p-3 rounded shadow'>
+                <Row className='mb-2'>
+                    <Col xl={6}>
+                        <Form.Group>
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control
+                                type='text'
+                                placeholder='Name'
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col xl={6}>
+                        <Form.Group>
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control
+                                type='text'
+                                placeholder='Address'
+                                ref={ref}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row className='my-2'>
+                    <Col xs={6} xxl={3}>
+                        <Form.Group>
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control
+                                type='tel'
+                                placeholder='Phone Number'
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col xs={6} xxl={3}>
+                        <Form.Group>
+                            <Form.Label>Cuisine</Form.Label>
+                            {getCuisines()}
+                        </Form.Group>
+                    </Col>
+                    <Col xxl={6}>
+                        <Form.Group>
+                            <Form.Label>Website</Form.Label>
+                            <Form.Control
+                                type='text'
+                                placeholder='Website'
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div className="m-3">
+                            <label className='m-3'>Upload image:</label>
+                            <input type="file" ref={inputRef} className='d-none'/>
+                            <button onClick={handleUpload} className='btn btn-primary'>Upload</button>
+                        </div>
+                    </Col>
+                </Row>
+                <Button variant='primary' type='submit' className='mt-2 rounded-pill shadow-sm'>
+                    Add truck
+                </Button>
+            </Form>
+        </Container>
     )
 }
--- src/pages/AddTruck.tsx
+++ src/pages/AddTruck.tsx
@@ -1,4 +1,6 @@
 import {FunctionComponent} from "react";
+import {Col, Container, Image, Row} from "react-bootstrap";
+import {AddTruckForm} from "../components/AddTruckForm";
 
 type AddTruckProps = {
 
@@ -6,6 +8,22 @@ type AddTruckProps = {
 
 export const AddTruck : FunctionComponent<AddTruckProps> = (props) => {
     return (
-        <div></div>
+        <Container fluid>
+            <Row className='mt-4'>
+                <Col xs={{span: 4, offset: 4}} md={{span: 2, offset: 5}}>
+                    <Image src='./logo.svg' width='100%' fluid />
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={{span: 6, offset: 3}} className='mt-4'>
+                    <h2 className='text-center fw-bold'>Add a truck</h2>
+                </Col>
+            </Row>
+            <Row className='mt-4'>
+                <Col md={{span: 8, offset: 2}}>
+                    <AddTruckForm />
+                </Col>
+            </Row>
+        </Container>
     )
 }
