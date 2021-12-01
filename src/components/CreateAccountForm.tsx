8aac69435e806a70c39dc05da7b3a3bd77d03ca2
Nick Gorjestani <nickg2794@hotmail.com>
2021-12-01 14:00:12 -0600

    Added state to create account form

--- src/components/CreateAccountForm.tsx
+++ src/components/CreateAccountForm.tsx
@@ -1,4 +1,4 @@
-import {FunctionComponent} from "react";
+import {FunctionComponent, useState} from "react";
 import {Button, Col, Form, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
@@ -7,39 +7,70 @@ type CreateAccountFormProps = {
 }
 
 export const CreateAccountForm : FunctionComponent<CreateAccountFormProps> = (props) => {
-    return (
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+        return (
         <Form className='bg-light p-3 rounded shadow'>
             <Row className='mb-2'>
                 <Col md={6}>
                     <Form.Group>
                         <Form.Label>First Name</Form.Label>
-                        <Form.Control type='text' placeholder='First Name' className='rounded-pill' />
+                        <Form.Control
+                            type='text'
+                            placeholder='First Name'
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                        />
                     </Form.Group>
                 </Col>
                 <Col md={6}>
                     <Form.Group>
                         <Form.Label>Last Name</Form.Label>
-                        <Form.Control type='text' placeholder='Last Name' className='rounded-pill' />
+                        <Form.Control
+                            type='text'
+                            placeholder='Last Name'
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
                     </Form.Group>
                 </Col>
             </Row>
             <Row className='my-2'>
                 <Form.Group>
                     <Form.Label>Email Address</Form.Label>
-                    <Form.Control type='email' placeholder='Email' className='rounded-pill' />
+                    <Form.Control
+                        type='email'
+                        placeholder='Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </Form.Group>
             </Row>
             <Row className='my-2'>
                 <Col md={6}>
                     <Form.Group>
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type='password' placeholder='Password' className='rounded-pill' />
+                        <Form.Control
+                            type='password'
+                            placeholder='Password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </Form.Group>
                 </Col>
                 <Col md={6}>
                     <Form.Group>
                         <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type='password' placeholder='Confirm Password' className='rounded-pill' />
+                        <Form.Control
+                            type='password'
+                            placeholder='Confirm Password'
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
                     </Form.Group>
                 </Col>
             </Row>
@@ -50,7 +81,11 @@ export const CreateAccountForm : FunctionComponent<CreateAccountFormProps> = (pr
             </Row>
             <Row className='mt-2'>
                 <Col xs='auto' className='text-center'>
-                    <Button variant='primary' type='submit' className='rounded-pill shadow-sm'>
+                    <Button
+                        variant='primary'
+                        type='submit'
+                        className='rounded-pill shadow-sm'
+                    >
                         Create Account
                     </Button>
                 </Col>
