7dca300bbeaf7fed3ceef6014a9e27a5dae41714
Nick Gorjestani <nickg2794@hotmail.com>
2021-12-01 13:33:05 -0600

    Added state to sign in form

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,4 +1,4 @@
-import {FunctionComponent} from "react";
+import {FunctionComponent, useState} from "react";
 import {Button, Container, Form} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
@@ -7,22 +7,39 @@ type SignInFormProps = {
 }
 
 export const SignInForm : FunctionComponent<SignInFormProps> = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <Container>
             <Form className='bg-light p-3 rounded shadow'>
                 <Form.Group className='mb-2'>
                     <Form.Label>Email Address</Form.Label>
-                    <Form.Control type='email' placeholder='Email' className='rounded-pill' />
+                    <Form.Control
+                        type='email'
+                        placeholder='Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </Form.Group>
                 <Form.Group className='my-2'>
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type='password' placeholder='Password' className='rounded-pill' />
+                    <Form.Control
+                        type='password'
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </Form.Group>
                 <Form.Text className='text-muted'>
                     Don't have an account? <Link to='create'>Create an account</Link>
                 </Form.Text>
                 <br />
-                <Button variant='primary' type='submit' className='mt-2 rounded-pill shadow-sm'>
+                <Button
+                    variant='primary'
+                    type='submit'
+                    className='mt-2 rounded-pill shadow-sm'
+                >
                     Submit
                 </Button>
             </Form>
