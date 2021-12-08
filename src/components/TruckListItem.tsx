1252f144e53248a8b192043ad05b6e1d163b1538
Nick Gorjestani <nickg2794@hotmail.com>
2021-12-08 16:13:21 -0600

    Truck detail page shows menu and contact info

--- src/components/TruckListItem.tsx
+++ src/components/TruckListItem.tsx
@@ -1,12 +1,20 @@
-import {FunctionComponent} from "react";
+import {FunctionComponent, MouseEventHandler} from "react";
 import FoodTruck from "../models/FoodTruck";
 import {Button, Card} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
 
 type TruckListItemProps = {
     truck: FoodTruck,
 }
 
 export const TruckListItem : FunctionComponent<TruckListItemProps> = ({truck}) => {
+    const navigate = useNavigate();
+
+    const handleDetailClick: MouseEventHandler = (e) => {
+        e.preventDefault();
+        navigate('/detail', {state: {foodTruck: truck}});
+    };
+
     return (
         <Card style={{backgroundColor: '#eee'}} className='border-0'>
             <Card.Img variant='top' src={truck.imageURL} style={{height: '160px'}} />
@@ -17,7 +25,7 @@ export const TruckListItem : FunctionComponent<TruckListItemProps> = ({truck}) =
                     Cuisine: {truck.cuisine}<br />
                     Phone: {truck.phone}<br />
                 </Card.Text>
-                <Button variant='secondary'>Details</Button>
+                <Button variant='secondary' onClick={handleDetailClick}>Details</Button>
             </Card.Body>
         </Card>
     )
--- src/pages/TruckDetail.tsx
+++ src/pages/TruckDetail.tsx
@@ -1,11 +1,71 @@
-import {FunctionComponent} from "react";
+import {FunctionComponent, useState} from "react";
+import {useLocation} from "react-router-dom";
+import {Card, Col, Container, Image, Row, Table} from "react-bootstrap";
+import MenuItem from "../models/MenuItem";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGlobe, faPhone} from "@fortawesome/free-solid-svg-icons";
 
 type TruckDetailProps = {
 
 }
 
 export const TruckDetail : FunctionComponent<TruckDetailProps> = (props) => {
+    const location = useLocation();
+    const [foodTruck, setFoodTruck] = useState(location.state.foodTruck);
+
+    const getTable = foodTruck.menu.map((item: MenuItem) => (
+        <tr>
+            <td>{item.name}</td>
+            <td>${item.price}</td>
+        </tr>
+    ));
+
     return (
-        <div></div>
+        <Container className='my-4' style={{backgroundColor: '#EEE'}}>
+            <Row>
+                <Col className='text-center my-2'>
+                    <h2 className='fw-bold'>{foodTruck.name}</h2>
+                    <h6>{foodTruck.address}</h6>
+                </Col>
+            </Row>
+            <Row className='my-2 justify-content-center'>
+                <Col md='auto'>
+                    <Image src={foodTruck.imageURL} rounded />
+                </Col>
+            </Row>
+            <Row className='justify-content-center my-3'>
+                <Col md='auto'>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Contact Info</Card.Title>
+                            <Card.Text>
+                                <FontAwesomeIcon icon={faGlobe} />: <a href={foodTruck.website}>{foodTruck.website}</a><br />
+                                <FontAwesomeIcon icon={faPhone} />: {foodTruck.phone}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col className='text-center'>
+                    <h4>Menu</h4>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>Item</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {getTable}
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+        </Container>
     )
 }
