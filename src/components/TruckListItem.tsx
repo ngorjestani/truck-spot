7f50436dd92e8eda0dd3b7303932286d16821c5a
Nick Gorjestani <nickg2794@hotmail.com>
2021-12-07 20:28:53 -0600

    Initial card style for results display

--- src/components/TruckListItem.tsx
+++ src/components/TruckListItem.tsx
@@ -1,11 +1,23 @@
 import {FunctionComponent} from "react";
+import FoodTruck from "../models/FoodTruck";
+import {Card} from "react-bootstrap";
 
 type TruckListItemProps = {
-
+    truck: FoodTruck,
 }
 
-export const TruckListItem : FunctionComponent<TruckListItemProps> = (props) => {
+export const TruckListItem : FunctionComponent<TruckListItemProps> = ({truck}) => {
     return (
-        <div></div>
+        <Card style={{width: '20rem'}}>
+            <Card.Img variant='top' src={truck.imageURL} />
+            <Card.Body>
+                <Card.Title>{truck.name}</Card.Title>
+                <Card.Text>
+                    {truck.address}<br />
+                    Cuisine: {truck.cuisine}<br />
+                    Phone: {truck.phone}<br />
+                </Card.Text>
+            </Card.Body>
+        </Card>
     )
 }
